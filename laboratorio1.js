c9243b7d937740db2d617ddc1c7b9faacd094dfa
OmarNino11 <omar.nino.parodi11@gmail.com>
2024-11-29 09:18:28 -0500

    inicio del preyecto firts commit

---
+++ laboratorio1.js
@@ -0,0 +1,41 @@
+//laboratorio 1 parte 1
+
+let nombre = "Omar";
+let edad = 23;
+let esEstudiante  = false;
+
+console.log  ( "nombre: " + nombre );
+console.log  ( "edad: " + edad );
+console.log  ( "es estudiante: " + esEstudiante );
+
+//laboratorio 1 parte 2
+
+let a = 9;
+let b = 3;
+
+let suma = a+b;
+let resta = a-b;
+let multi = a*b;
+let divi = a/b;
+
+console.log  ( "suma: " + suma );
+console.log  ( "resta: " + resta );
+console.log  ( "multiplicar: " + multi );
+console.log  ( "dividir: " + divi );
+
+//laboratorio 1 parte 3
+
+let esMayorDeEdad =  true;
+let tieneLicensia = false;
+
+console.log('laboratorio1 - ejercisio3')
+
+console.log  ( "Es mayor de edad?  " + esMayorDeEdad );
+console.log  ( "Tiene licensia? " + tieneLicensia );
+
+if(esMayorDeEdad  && tieneLicensia)
+    console.log('la persona puede conducir')
+else
+    console.log('la persona No puede conducir')
+
+console.log(`es mayor de edad: ${esMayorDeEdad}`);
